9b4385a26538b51115548455d2a8cc3ee556856a
swpsh <sfgussf@naver.com>
2025-10-16 16:30:51 +0900

    123

--- 정보A/5차시 클래스 및 parent 이해.py
+++ 정보A/5차시 클래스 및 parent 이해.py
@@ -8,17 +8,23 @@ def input(key):
     if key == 'escape':
         application.quit()
 
-sun_texture = load_texture('textures\sun.jpg')
+class Planet(Entity):
+    def __init__(self, name, texture, position, scale, rotation_speed):
+        super().__init__(
+            model = 'sphere',
+            texture = texture,
+            position = position,
+            scale = scale,
+            rotation_speed = rotation_speed
+        )
+        self.name = name
+
+    def update(self):
+        self.rotation_y += self.rotation_speed * time.dt
+
+sun_texture = load_texture('textures/sun.jpg')
 
 
-sun = Entity(
-    model = 'sphere',
-    texture = sun_texture,
-    color = color.white,
-    scale = 5,
-    position = (0,0,0)
-)
-
-
+Planet('Sun', sun_texture, (3,0,0), (5, 5, 5), 47.87)
 
 app.run()
