e5d1ddd94bf40c3c2b0c1e1d8f3abe4d1cba91a0
swpsh <sfgussf@naver.com>
2025-10-24 12:57:43 +0900

    123123

--- 정보C/5차시 class와 parent 에 대한 이해.py
+++ 정보C/5차시 class와 parent 에 대한 이해.py
@@ -9,17 +9,31 @@ def input(key):
         application.quit()
 
 class Planet(Entity): ## 행성을 만드는 클래스를 선언한다.
-    def __init__(self, texture, pos, scale):
+    def __init__(self, texture, pos, scale, orbit = 5):
         super().__init__(
             model = 'sphere',
             texture = texture,
             position = pos,
-            scale = scale
+            scale = scale,
+            orbit = orbit
         )
+        self.pivot()
+
+    def pivot(self, angle=25):
+        self.pi = Entity(
+            position = (0,0,0),
+            rotation_x = angle
+        )
+        self.parent = self.pi
+    
+
+    def update(self):
+        self.rotation_y += 20 * time.dt #행성의 자전 속도 설정
+        self.pi.rotation_y += self.orbit #행성의 공전 속도 설정
 
 Planet(texture = 'textures/sun.jpg', pos = (0, 0, 0), scale = 5)
-Planet(texture = 'textures/earth.jpg', pos = (5, 0, 0), scale = 5)
-Planet(texture = 'textures/mars.jpg', pos = (10, 0, 0), scale = 5)
+Planet(texture = 'textures/earth.jpg', pos = (55, 0, 0), scale = 5)
+Planet(texture = 'textures/mars.jpg', pos = (105, 0, 0), scale = 5)
 
 
 
