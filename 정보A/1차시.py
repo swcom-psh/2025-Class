acaee16d300cffad94cf36ead70b3e2f2af052e2
swpsh <sfgussf@naver.com>
2025-11-03 10:52:00 +0900

    123

--- 정보A/1차시.py
+++ 정보A/1차시.py
@@ -10,26 +10,5 @@ def input(key):
 
 cube = Entity(model = 'cube', scale = 5, position = (0,0,0), color = color.red, blink = True)
 
-Button(
-    text='HI', 
-    parent=camera.ui, 
-    model=Default, 
-    radius=Default, 
-    origin=(0, 0), 
-    texture=Default, 
-    color=Default, 
-    collider='box', 
-    text_color=Default, 
-    text_origin=(0, 0), 
-    text_size=1, 
-    highlight_text_size=None, 
-    highlight_text_color=None, 
-    highlight_scale=1, 
-    pressed_scale=1, 
-    disabled=False
-    )
-
-
-
 app.run()
 
--- 정보A/2-3차시 물체 이동시키기.py
+++ 정보A/2-3차시 물체 이동시키기.py
@@ -1,37 +1,49 @@
 from ursina import *
+#우르시나 라이브러리의 모든 기능(=*)을 가져온다.
 import random as ran
+
 
 app = Ursina()
 EditorCamera()
 
-objects = 2000
+objects = 300
+
 somethings = []
 
 def input(key):
     if key == 'escape':
         application.quit()
 
-for i in range(objects):
-    pos = (ran.uniform(-100,100), ran.uniform(-100,100), ran.uniform(-100,100))#튜플 이라고 하는 자료형
-    a = Entity(
-        model = 'sphere',
-        color = color.random_color(),
-        position = pos,
-        scale = 5
-    )
-    a.dis = Vec3(ran.uniform(-10,10), ran.uniform(-10,10), ran.uniform(-10,10))#튜플 이라고 하는 자료형 
-    somethings.append(a)
-    
-height = 100
+for obj in range(objects):
+    pos = Vec3(ran.uniform(-100,100), 0, ran.uniform(-100,100))
 
-def update():#예약함수 중 하나로, 1초에 60프레임// 1초에 60회 반복해주는 함수
+    cube = Entity(
+        model = 'sphere', #속성
+        position = pos,
+        scale = 5,
+        color = color.random_color()
+    )
+    cube.dirs = Vec3(ran.uniform(-10,10), 0, ran.uniform(-10,10))
+    #큐브 객체의 dirs 속성을 만들어주었다.
+    #우르시나에서만 쓰는 자료형: Vec3 >> 벡터형태의 3차원(x,y,z 좌표)자료형
+
+    somethings.append(cube) #큐브 객체를 썸띵리스트에 저장
+    #각 엔티티의 정보가 담기겠죠.
+
+def update():
     for some in somethings:
-        some.position += some.dis
-        #some.position = some.position + ~~
-        if abs(some.x) + abs(some.z) > (height - some.y) or a.y < 0 or a.y > height:
-            some.dis *= -1
+        some.position += some.dirs * time.dt
+
+        if abs(some.x) > (some.x)*(some.x) + (some.z)*(some.z): #abs() 절대값을 출력해주는 함수
+            some.dirs.x *= -1
+        if abs(some.z) > (some.x)*(some.x) + (some.z)*(some.z): #abs() 절대값을 출력해주는 함수
+            some.dirs.z *= -1   
+
+
+
     
-        
+
+
+
 
 app.run()
-
